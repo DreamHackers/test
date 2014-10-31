f207da90e15a9980bf80fed29bb0e26b80d8c16e
Seed <cb.so.devope+0001@gmail.com>
2014-10-31 22:35:07 +0900

    Create CallByReference.java

---
+++ fxxkCode/CallByReference.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CallByReference.java {
+
+    public static void main(String[] args) {
+        System.out.println(111111);
+        fxxk1();
+    }
+
+    private static void fxxk1() {
+        Map<Integer, String> fxxk = new HashMap<Integer, String>(2);
+        fxxk.put(0, "素晴らしい格納");
+        fxxk(fxxk);
+        int i = 0;
+        while (i < fxxk.size()) {
+            System.out.println(fxxk.get(i));
+            i++;
+        }
+    }
+
+    public static void fxxk(Map<Integer, String> map) {
+        map.put(0, "fxxk格納");
+        map.put(1, "fxxk2格納");
+    }
+
+}
