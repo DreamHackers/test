7cb659ec75176193b6090a90e362c5de90d83216
Seed <cb.so.devope+0001@gmail.com>
2014-11-01 00:01:11 +0900

    クラス名修正

--- fxxkCode/CallByReference.java
+++ fxxkCode/CallByReference.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class CallByReference.java {
+public class CallByReference {
 
     public static void main(String[] args) {
         System.out.println(111111);
